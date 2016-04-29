6671a1d57f21c4743e0c619d9eb59cf80073d290
enio9999@yahoo.com.br <enio9999@yahoo.com.br>
2016-04-28 21:36:11 -0300

    Implementando angular.

--- loteria-app.js
+++ loteria-app.js
@@ -1,1 +1,12 @@
-angular.module();
+(function (angular){
+	angular
+	.module('loteriaApp', [])
+	.controller('loteriaController', loteriaController);
+
+	function loteriaController($scope){
+		var vm = this;
+		
+		vm.name = 'teste';
+	};
+})(angular);
+

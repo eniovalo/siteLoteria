2bfece78918612daef27a54301467752df4a9d84
enio9999@yahoo.com.br <enio9999@yahoo.com.br>
2016-05-30 21:43:00 -0300

    Implementacao da geracao de jogos atraves da data atual.

--- loteria-app.js
+++ loteria-app.js
@@ -6,10 +6,58 @@
 
 	function LoteriaController($scope){
 		var vm = this;
-
-        console.log('teste');
 		
-		vm.name = 'teste';
+		//atributos.
+		vm.jogo = {
+			menorNumero: 1,
+			maiorNumero: 60,
+			quantidadeNumeros: 6
+		};
+		vm.jogoGerado = null;
+		
+		//Metodos.
+		vm.gerarJogo = gerarJogo;
+		
+		function gerarJogo() {
+			vm.jogoGerado = [];
+			var dataAtual = new Date();
+			
+			adicionarNumero(dataAtual.getMilliseconds());
+			
+			adicionarNumero(dataAtual.getMilliseconds() * 
+							dataAtual.getSeconds());
+							
+			adicionarNumero(dataAtual.getMilliseconds() * 
+							dataAtual.getSeconds() * 
+							dataAtual.getMinutes());
+							
+			adicionarNumero(dataAtual.getMilliseconds() * 
+							dataAtual.getSeconds() * 
+							dataAtual.getMinutes() *
+							dataAtual.getHours());
+							
+			adicionarNumero(dataAtual.getMilliseconds() * 
+							dataAtual.getSeconds() * 
+							dataAtual.getMinutes() *
+							dataAtual.getHours() *
+							dataAtual.getDate());
+
+			adicionarNumero(dataAtual.getMilliseconds() * 
+							dataAtual.getSeconds() * 
+							dataAtual.getMinutes() *
+							dataAtual.getHours() *
+							dataAtual.getDate() *
+							(dataAtual.getMonth()+1) );
+		}
+		
+		function validarNumero(numeroValidacao) {
+			return numeroValidacao == 0 ? vm.jogo.maiorNumero : numeroValidacao;
+		}
+		
+		function adicionarNumero(numeroAdicao) {
+			vm.jogoGerado.push(validarNumero(numeroAdicao % vm.jogo.maiorNumero));
+		}
 	}
 
 })(angular);
+

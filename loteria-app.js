1b7f1550d7ef267b981e9c9b1e3cee8d7af8654a
Enio <enio9999@yahoo.com.br>
2016-06-06 21:38:15 -0300

    Aprimoramento da geração de jogos.

--- loteria-app.js
+++ loteria-app.js
@@ -6,6 +6,9 @@
 
 	function LoteriaController($scope){
 		var vm = this;
+
+        //constantes
+        var NAO_ENCONTRADO = -1;
 		
 		//atributos.
 		vm.jogo = {
@@ -23,42 +26,68 @@
 			vm.jogoGerado = [];
 			var dataAtual = new Date();
 
+            //Milisegundos
 			adicionarNumero(dataAtual.getMilliseconds());
 			
+            //Segundos
 			adicionarNumero(dataAtual.getMilliseconds() * 
 							dataAtual.getSeconds());
-							
+
+            //Minutos
 			adicionarNumero(dataAtual.getMilliseconds() * 
 							dataAtual.getSeconds() * 
 							dataAtual.getMinutes());
-							
+
+            //Horas
 			adicionarNumero(dataAtual.getMilliseconds() * 
 							dataAtual.getSeconds() * 
 							dataAtual.getMinutes() *
 							dataAtual.getHours());
-							
+
+            //Dia
 			adicionarNumero(dataAtual.getMilliseconds() * 
 							dataAtual.getSeconds() * 
 							dataAtual.getMinutes() *
 							dataAtual.getHours() *
 							dataAtual.getDate());
 
+            //Mês
 			adicionarNumero(dataAtual.getMilliseconds() * 
 							dataAtual.getSeconds() * 
 							dataAtual.getMinutes() *
 							dataAtual.getHours() *
 							dataAtual.getDate() *
 							(dataAtual.getMonth()+1) );
+
+            //Caso a quantidade de jogos não tenha sido completada,
+            //completo com base nos milisegundos.
+            while(vm.jogoGerado.length < vm.jogo.quantidadeNumeros) {
+                dataAtual = new Date();
+                adicionarNumero(dataAtual.getMilliseconds() *
+                                dataAtual.getMilliseconds());
+            }
 		}
 		
-		function validarNumero(numeroValidacao) {
-			return numeroValidacao === 0 ? vm.jogo.maiorNumero : numeroValidacao;
+		function tratarNumero(numero) {
+            //Utilizo apenas os números que estão no range do jogo.
+            var numeroGerado = numero % vm.jogo.maiorNumero;
+
+			return (numeroGerado === 0) ? vm.jogo.maiorNumero : numeroGerado;
+		}
+
+        function validarNumero(numero) {
+            //Não permito adicionar números duplicados.
+			return vm.jogoGerado.indexOf(numero) === NAO_ENCONTRADO;
 		}
 		
-		function adicionarNumero(numeroAdicao) {
-			vm.jogoGerado.push(validarNumero(numeroAdicao % vm.jogo.maiorNumero));
+		function adicionarNumero(numero) {
+            var numeroAdicao = tratarNumero(numero);
+
+            if(validarNumero(numeroAdicao)) {
+                vm.jogoGerado.push(numeroAdicao);
+            }
 		}
+
 	}
 
 })(angular);
-
